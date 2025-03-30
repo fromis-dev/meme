f8cc8e76947cb2fd9dabb625488319a0be91a419
fromis-dev <fromis_dev@naver.com>
2025-03-31 00:57:36 +0900

    Update sounds.js

--- kane/11su/modules/sounds.js
+++ kane/11su/modules/sounds.js
@@ -1,11 +1,11 @@
 export const sounds = {
-  intro: new Audio("../sounds/intro.mp3"), // 링크를 클릭하셨군요?
-  bgm: new Audio("../sounds/miro.mp3"), // 미로마을 오때론난망가져갈지도모르지
-  over: new Audio("../sounds/over.mp3"), // 땡땡땡~
+  intro: new Audio("../11su/sounds/intro.mp3"), // 링크를 클릭하셨군요?
+  bgm: new Audio("../11su/sounds/miro.mp3"), // 미로마을 오때론난망가져갈지도모르지
+  over: new Audio("../11su/sounds/over.mp3"), // 땡땡땡~
   koEffect: [
-    new Audio("../sounds/nice1.mp3"),
-    new Audio("../sounds/nice2.mp3"),
-    new Audio("../sounds/nice3.mp3"),
-    new Audio("../sounds/nice4.mp3"),
+    new Audio("../11su/sounds/nice1.mp3"),
+    new Audio("../11su/sounds/nice2.mp3"),
+    new Audio("../11su/sounds/nice3.mp3"),
+    new Audio("../11su/sounds/nice4.mp3"),
   ],
 };
